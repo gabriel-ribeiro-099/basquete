ca137f6f7aa21d2a9d9cae2f96f059073bd88850
BiancaM <biancamm0107@gmail.com>
2025-09-06 01:28:34 -0300

    feat: timer + periodo + novo layout

--- app/src/main/java/com/example/basquete/MainActivity.kt
+++ app/src/main/java/com/example/basquete/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.basquete
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -8,17 +9,26 @@ import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     private var pontuacaoTimeA: Int = 0
+    private var periodo: Int = 0
     private var pontuacaoTimeB: Int = 0
 
-    private lateinit var pTimeA: TextView
-    private lateinit var pTimeB: TextView
+    private lateinit var pTimeATextView: TextView
+    private lateinit var pTimeBTextView: TextView
+    private lateinit var periodoTextView: TextView
+    private lateinit var tempoJogoTextView: TextView
+    private lateinit var iniciarJogoButton: Button
+
+    private var countDownTimer: CountDownTimer? = null
+    private var isGameStarted = false
+    private val tempoDoPeriodoEmMilissegundos: Long = 10 * 1000 //dps coloca 10 minutos
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        pTimeA = findViewById(R.id.placarTimeA)
-        pTimeB = findViewById(R.id.placarTimeB)
+        pTimeATextView = findViewById(R.id.placarTimeA)
+        pTimeBTextView = findViewById(R.id.placarTimeB)
+        periodoTextView = findViewById(R.id.periodo)
 
         val bTresPontosTimeA: Button = findViewById(R.id.tresPontosA)
         val bDoisPontosTimeA: Button = findViewById(R.id.doisPontosA)
@@ -26,8 +36,19 @@ class MainActivity : ComponentActivity() {
         val bTresPontosTimeB: Button = findViewById(R.id.tresPontosB)
         val bDoisPontosTimeB: Button = findViewById(R.id.doisPontosB)
         val bTLivreTimeB: Button = findViewById(R.id.tiroLivreB)
-        val bReiniciar: Button = findViewById(R.id.reiniciarPartida)
 
+        tempoJogoTextView = findViewById(R.id.tempoJogo)
+        iniciarJogoButton = findViewById(R.id.iniciarJogo)
+
+        iniciarJogoButton.setOnClickListener {
+            if (!isGameStarted) {
+                reiniciarPartida()
+                isGameStarted = true
+            }
+            periodo += 1
+            periodoTextView.setText(periodo.toString())
+            iniciarTimer()
+        }
         bTresPontosTimeA.setOnClickListener {
             adicionarPontos(3, "A")
         }
@@ -46,9 +67,32 @@ class MainActivity : ComponentActivity() {
         bTLivreTimeB.setOnClickListener {
             adicionarPontos(1, "B")
         }
-        bReiniciar.setOnClickListener {
-            reiniciarPartida()
-        }
+    }
+
+    private fun iniciarTimer() {
+        countDownTimer?.cancel()
+
+        countDownTimer = object : CountDownTimer(tempoDoPeriodoEmMilissegundos, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                val segundosRestantes = millisUntilFinished / 1000
+                val minutos = segundosRestantes / 60
+                val segundos = segundosRestantes % 60
+                val tempoFormatado = String.format("%02d:%02d", minutos, segundos)
+                tempoJogoTextView.text = tempoFormatado
+            }
+
+            override fun onFinish() {
+                tempoJogoTextView.text = "00:00"
+                if(periodo == 4){
+                    isGameStarted = false;
+                    iniciarJogoButton.text = "Iniciar Novo Jogo"
+                }
+                if (isGameStarted) {
+                    iniciarJogoButton.text = "Iniciar Novo Período"
+                }
+            }
+        }.start() // Inicia o timer
     }
 
     fun adicionarPontos(pontos: Int, time: String) {
@@ -62,17 +106,19 @@ class MainActivity : ComponentActivity() {
 
     fun atualizarPlacar(time: String){
         if (time == "A") {
-            pTimeA.setText(pontuacaoTimeA.toString())
+            pTimeATextView.setText(pontuacaoTimeA.toString())
         } else {
-            pTimeB.setText(pontuacaoTimeB.toString())
+            pTimeBTextView.setText(pontuacaoTimeB.toString())
         }
     }
 
     fun reiniciarPartida() {
         pontuacaoTimeA = 0
-        pTimeA.setText(pontuacaoTimeA.toString())
+        pTimeATextView.setText(pontuacaoTimeA.toString())
         pontuacaoTimeB = 0
-        pTimeB.setText(pontuacaoTimeB.toString())
+        pTimeBTextView.setText(pontuacaoTimeB.toString())
+        periodo = 0
+        periodoTextView.setText(periodo.toString())
         Toast.makeText(this,"Placar reiniciado",Toast.LENGTH_SHORT).show()
     }
 }
